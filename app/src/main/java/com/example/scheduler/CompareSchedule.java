82050b799eb3cd255f4053378631b4d3e5c0cf7d
sarashahid <sshahid3@illinois.edu>
2019-04-29 07:41:12 -0500

    Created new CompareSchedule class

---
+++ app/src/main/java/com/example/scheduler/CompareSchedule.java
@@ -0,0 +1,13 @@
+package com.example.scheduler;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class CompareSchedule {
+    /**
+     * Compare two calendars.
+     */
+    public List<Event> syncCalendars(List<Event> first, List<Event> second) {
+        List<Event> synced = new ArrayList<Event>();
+    }
+}
